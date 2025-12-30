1fc3c7adcf0ec3d8c15f998f831ec46156c12b8d
johnny17V <jjasenghor@ept.sn>
2025-12-30 22:53:10 +0000

    éplacer la balise header du cv dans le composant en-tête puis mettre la balise

--- cv_bootstrap/src/app/app.ts
+++ cv_bootstrap/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Entete } from './entete/entete';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Entete],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
