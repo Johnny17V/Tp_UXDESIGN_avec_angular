e13a5d45081138d456acebfd28ba26e8ac2c4f09
johnny17V <jjasenghor@ept.sn>
2025-12-31 12:31:39 +0000

    Ajout du composant contact

--- cv_bootstrap/src/app/app.ts
+++ cv_bootstrap/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { Entete } from './entete/entete';
+import { Contact } from './contact/contact';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Entete],
+  imports: [RouterOutlet, Entete, Contact],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
